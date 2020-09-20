1b3976c30851c1fb0ee87d15c033fa8de0b57045
tonyscofieldkim <scofieldtony25@gmail.com>
2020-09-20 18:11:02 +0300

    Format code

--- upload-from-computer.php
+++ upload-from-computer.php
@@ -77,13 +77,13 @@ $current_level = get_current_user_level();
 					multipart : true,
 					<?php
 						if ( false === CAN_UPLOAD_ANY_FILE_TYPE ) {
-					?>
+					?>,
 							filters : [
 								{title : "Allowed files", extensions : "<?php echo $options_values['allowed_file_types']; ?>"}
 							],
 					<?php
 						}
-					?>
+					?>,
 					flash_swf_url : 'includes/plupload/js/plupload.flash.swf',
 					silverlight_xap_url : 'includes/plupload/js/plupload.silverlight.xap',
 					preinit: {
