df3dc23d77f85db97bc0da11c617e7b13d6624be
tonyscofieldkim <scofieldtony25@gmail.com>
2020-10-19 22:35:47 +0300

    Add metadata endpoint

---
+++ sso/pingfed/sso_sp_metadata.php
@@ -0,0 +1,25 @@
+<?php
+ 
+/**
+ *  SP Metadata Endpoint
+ */
+require_once('../../sys.includes.php');
+require_once('sp_settings/settings.php');
+
+try {
+    $auth = new OneLogin_Saml2_Auth($advancedSettings);
+    $settings = $auth->getSettings();
+    $metadata = $settings->getSPMetadata();
+    $errors = $settings->validateMetadata($metadata);
+    if (empty($errors)) {
+        header('Content-Type: text/xml');
+        echo $metadata;
+    } else {
+        throw new OneLogin_Saml2_Error(
+            'Invalid SP metadata: '.implode(', ', $errors),
+            OneLogin_Saml2_Error::METADATA_SP_INVALID
+        );
+    }
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
