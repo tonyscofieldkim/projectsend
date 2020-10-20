628e1dce2e5f712170238c623b57bef0441e2833
tonyscofieldkim <scofieldtony25@gmail.com>
2020-10-21 02:32:21 +0300

    Complete SSO Endpoint, IDP Initiated

--- sso/pingfed/sso2.php
+++ sso/pingfed/sso2.php
@@ -4,11 +4,26 @@
  * Asertion consumption Endponit for SSO
  */
 
-session_start();
-session_regenerate_id();
-
 require_once('../../sys.includes.php');
 require_once('sp_settings/settings.php');
+
+if (!defined('SAML2_SSO_ENABLED') || SAML2_SSO_ENABLED != '1') {
+    echo '<p>SSO Method is off</p>';
+    exit();
+}
+
+try {
+    //ob_start();
+    //code...
+    //session_start();
+    $_SESSION['REGEN_XID'] = 0;
+    session_regenerate_id(false);
+    unset($_SESSION['REGEN_XID']);
+} catch (\Throwable $th) {
+    //throw $th;
+}
+$allowed_levels = array(9, 8, 7, 0);
+$defaultPassword = 'P.a.ss*W.or.d01#';
 
 $auth = new OneLogin_Saml2_Auth($advancedSettings);
 if (isset($_SESSION) && isset($_SESSION['AuthNRequestID'])) {
@@ -22,14 +37,17 @@ $auth->processResponse($requestID);
 $errors = $auth->getErrors();
 
 if (!empty($errors)) {
-    echo '<p>', implode(', ', $errors), '</p>'; //not good to print these errors to user
+    echo '<p>Errors occured at SSO response.</p>';
+    //echo '<p>', implode(', ', $errors), '</p>'; //not good to print these errors to user
 }
 
 if (!$auth->isAuthenticated()) {
-    echo "<p>Not authenticated</p>";
+    echo "<p>Sorry, you were not authenticated.</p>";
     exit();
 }
-//check if the user exist
+
+
+$_SESSION['IdPSessionIndex'] = $auth->getSessionIndex();
 $attributes = $auth->getAttributes();
 $nameId = $auth->getNameId();
 $nameIdFormat = $auth->getNameIdFormat();
@@ -38,10 +56,183 @@ $nameIdServiceNameQualifier = $auth->getNameIdSPNameQualifier();
 $sessionIndex = $auth->getSessionIndex();
 unset($_SESSION['AuthNRequestID']);
 
+//check for attributes
+$attributesNames = array(
+    SAML2_ATTR_EMAIL,
+    SAML2_ATTR_ROLE,
+    SAML2_ATTR_GIVENNAMES
+);
+$attributeEmail = false;
+$attributeRole = false;
+$attributeGivenNames = false;
+foreach ($attributes as $attributeName => $attributeValue) {
+    if ($attributeName == $attributesNames[0]) $attributeEmail = $attributeValue;
+    if ($attributeName == $attributesNames[1]) $attributeRole = $attributeValue;
+    if ($attributeName == $attributesNames[2]) $attributeGivenNames = $attributeValue;
+}
+
+if (empty($attributeEmail) || empty($attributeRole) || empty($attributeGivenNames)) {
+    //incorrect assertions.
+    echo "<p>You were not authenticated properly. Some Identity Assertion Attributes are missing.</p>";
+    exit;
+}
 
 
+//check if this user exists or not.
+$statement = $dbh->prepare("SELECT * FROM " . TABLE_USERS . " WHERE (user= :username OR email= :email) AND level = :level");
+if (array_key_exists(intval($attributeRole), $allowed_levels)) {
+    $attributeRole = intval($attributeRole);
+} else {
+    $attributeRole = 0; //defaults to client user.
+}
 
-//last if might be redundant
+$statement->execute(
+    array(
+        ':username'    => $nameId,
+        ':email'    => $attributeEmail,
+        ':level' => $attributeRole
+    )
+);
+$count_user = $statement->rowCount();
+if ($count_user > 0) {
+    //check if nameId has matched
+    $userInfo = $statement->fetch(PDO::FETCH_ASSOC);
+    if ($userInfo['user'] == $nameId) {
+        //this user is already created by vsmr, login the user
+        if ($userInfo['active'] != 0) {
+            $access_string = 'admin';
+            /** Set SESSION values */
+            $_SESSION['loggedin']  = html_output($userInfo['user']);
+            $_SESSION['userlevel']  = $userInfo['level'];
+            $_SESSION['lang'] = SITE_LANG;
+
+            if ($userInfo['level'] != 0) {
+                $access_string    = 'admin';
+                $_SESSION['access']  = $access_string;
+            } else {
+                $access_string    = html_output($userInfo['user']);
+                $_SESSION['access']  = html_output($userInfo['user']);
+            }
+
+            /** Record the action log */
+            $new_log_action = new LogActions();
+            $log_action_args = array(
+                'action' => 1,
+                'owner_id' => $userInfo['id'],
+                'owner_user' => $userInfo['name'],
+                'affected_account_name' => $userInfo['name']
+            );
+            $new_record_action = $new_log_action->log_action_save($log_action_args);
+        } else {
+            echo "<p>Your account is deactivated</p>";
+            exit;
+        }
+    } else {
+        if ($userInfo['email'] == $attributeEmail) {
+            //this user is created but not via vsmr. should edit his user nameId and Name and log them in
+            $statement = $dbh->prepare("UPDATE " . TABLE_USERS . " SET user = :user, name = :name WHERE email = :email");
+            $statement->execute(array(
+                ':user' => $nameId,
+                ':name' => $attributeGivenNames
+            ));
+            if ($statement->rowCount() < 1) {
+                echo "<p>Your account could not be connected. Error code DBERR_QUERYFAIL</p>";
+                exit;
+            }
+
+            if ($userInfo['active'] != 0) {
+                $access_string = 'admin';
+                /** Set SESSION values */
+                $_SESSION['loggedin']  = html_output($userInfo['user']);
+                $_SESSION['userlevel']  = $userInfo['level'];
+                $_SESSION['lang'] = SITE_LANG;
+
+                if ($userInfo['level'] != 0) {
+                    $access_string    = 'admin';
+                    $_SESSION['access']  = $access_string;
+                } else {
+                    $access_string    = html_output($userInfo['user']);
+                    $_SESSION['access']  = html_output($userInfo['user']);
+                }
+
+                /** Record the action log */
+                $new_log_action = new LogActions();
+                $log_action_args = array(
+                    'action' => 1,
+                    'owner_id' => $userInfo['id'],
+                    'owner_user' => $userInfo['name'],
+                    'affected_account_name' => $userInfo['name']
+                );
+                $new_record_action = $new_log_action->log_action_save($log_action_args);
+            } else {
+                echo "<p>Your account is deactivated</p>";
+                exit;
+            }
+        }
+    }
+} else {
+    //this is a new user and is registered via vsmr. register them and log in
+    //find if we have this email already
+    $statement = $dbh->prepare("SELECT COUNT * AS counted FROM " . TABLE_USERS . " WHERE email = :email");
+    $statement->execute(array(
+        ':email' => $attributeEmail
+    ));
+    $count_user_x = $statement->rowCount();
+    if ($count_user > 0) {
+        if ($count_user_x['counted'] > 0) {
+            //user exists.
+            echo "<p>Could create account. This email is already taken. ($attributeEmail)</p>";
+            exit;
+        }
+    } else {
+        echo "<p>Could not log in. Internal error code is: DBERR_QUERYFAIL</p>";
+        exit;
+    }
+    //proceed with registration
+    $new_user = new UserActions();
+    $new_arguments = array(
+        'id' => '',
+        'username' => $nameId,
+        'password' => $defaultPassword,
+        //'password_repeat' => $_POST['add_user_form_pass2'],
+        'name' => $attributeGivenNames,
+        'email' => $attributeEmail,
+        'role' => $attributeRole,
+        'active' => 1,
+        'max_file_size'    => 500,
+        'notify_account' => 0,
+        'type' => 'new_user'
+    );
+
+    /** Validate the information from the posted form. */
+    $new_validate = $new_user->validate_user($new_arguments);
+
+    /** Create the user if validation is correct. */
+    if ($new_validate == 1) {
+        $new_response = $new_user->create_user($new_arguments);
+    } else {
+        echo "Could not create your account. One or more parameters are invalid";
+        exit;
+    }
+    $access_string = 'admin';
+    /** Set SESSION values */
+    $_SESSION['loggedin']  = html_output($nameId);
+    $_SESSION['userlevel']  = $attributeRole;
+    $_SESSION['lang'] = SITE_LANG;
+
+    if ($userInfo['level'] != 0) {
+        $access_string    = 'admin';
+        $_SESSION['access']  = $access_string;
+    } else {
+        $access_string    = html_output($userInfo['user']);
+        $_SESSION['access']  = html_output($userInfo['user']);
+    }
+
+}
+
 if (isset($_POST['RelayState']) && OneLogin_Saml2_Utils::getSelfURL() != $_POST['RelayState']) {
     $auth->redirectTo($_POST['RelayState']);
+} else {
+    $location_ = BASE_URI . 'home.php';
+    header("Location: $location_");
 }
